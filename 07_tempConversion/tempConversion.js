b08ecfa6ad59d435041adb7fa1adf858b8d21466
Burak <avniburakcitlak@gmail.com>
2022-07-14 19:58:01 +0300

    finished 6 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(fah) {
+  let res=(fah-32)*(5/9);
+  return Math.round(res*10)/10;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let res=cel*(9/5)+32;
+  return Math.round(res*10)/10;
 };
 
 // Do not edit below this line
